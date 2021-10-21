81924e26f84fcc85c8953ed66f473bc764079f11
decode11 <91780804+decode11@users.noreply.github.com>
2021-10-22 00:12:40 +0530

    Create apple.js

---
+++ apple.js
@@ -0,0 +1,14 @@
+function Apple(color,score)
+{
+  this.color=color;
+  this.score=score;
+  
+}
+
+Apple.prototype={
+ eat:function(){ return "eat";},
+ throw :function(){ return "throw" ;}
+};
+
+var app_1=new Apple("red",50);
+var app_21=new Apple("green",78);
